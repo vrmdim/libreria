311ffaa13c84ee149354ff27694a4d3122cdfb4c
vrmdim <vrmdim45@gmail.com>
2023-03-07 12:44:23 +0100

    libreria publica

---
+++ src/main/java/com/example/demo/SpringLibreriaApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+
+public class SpringLibreriaApplication {
+
+	public static void main(String[] args) {
+	}
+	
+	
+	public static void mostrarMensaje(){
+		
+		System.out.println("Ejecutando mensaje desde la librería");
+		
+	}
+
+}
